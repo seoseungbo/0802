ae2d671470c5b3f81e986c9692e3866f963123ec
User <pgx30116@gmail.com>
2022-08-03 09:26:07 +0900

    0802 자바 수업

--- src/Practice.java
+++ src/Practice.java
@@ -1,15 +1,36 @@
 
 public class Practice {
 	public static void main(String[] args) {
-		int [] result = {1,2,3,4,1,2,3,4,1,2}
-		int [] array = {{1,2,2,4,2,2,3,4,1,2}, {1,2,3,4,1,1,1,4,1,1}, {1,2,3,4,1,2,3,4,1,2}, {1,3,3,3,3,3,3,3,1,2}, {1,2,3,4,1,2,3,1,1,2}};
+		int [] answer = {1,2,3,4,1,2,3,4,1,2};
+		String [] names = {"한지민", "박지민", "홍지민", "신지민", "김지민"};
 		
-		int score = 0;
-		int count = 0;
-		for(int i = 0; i < 5; i++) {
-			switch (i)
+		int [][] array = {
+				{1,2,2,4,2,2,3,4,1,2}, 
+				{1,2,3,4,1,1,1,4,1,1}, 
+				{1,2,3,4,1,2,3,4,1,2}, 
+				{1,3,3,3,3,3,3,3,1,2}, 
+				{1,2,3,4,1,2,3,1,1,2}
+		};
+		char [][] results = new char [names.length][10];
+		int [] jumsus = new int[names.length];
+		for(int i = 0; i < results.length; i++) {
+			int jumsu = 0;
+			for(int j = 0; j < results[i].length; j++) {
+				if(array[i][j] == answer[j]) {
+					results[i][j] = 'O'; jumsu += 10;
+				}
+			}
+			else results[i][j] = 'X';
 		}
+			jumsus[i] = jumsu;
+		}
+		for(int i = 0 ; i <names.length; i++) {
+			System.out.println(names[i] + " : ");
+			for(int j = 0 ; j < results[i].length ; j++);
+			System.out.println("%c\t", results[i][j]);
+		}
+		System.out.println("(")
+		
 			
 		
 	}
-}
